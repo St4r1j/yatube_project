f738e01d62ed6eed6fdad30de4f0a32f78bc3417
Annenkov Daniil <annenkovdaniil.l@gmail.com>
2025-09-13 15:19:18 +0300

    added models and some editions

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'title': title
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+        'group': group
+    }
+    return render(request, template, context)
